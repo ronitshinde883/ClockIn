538751be5c54cb62ba6bd964f6a05ccedabc702f
Ronit Shinde <shinderonit4@gmail.com>
2026-09-02 21:27:08 +0530

    Added models

--- backend/ClockIn/api/admin.py
+++ backend/ClockIn/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from api.models import User,StudentProfile,TeacherProfile,Department,College
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(StudentProfile)
+admin.site.register(TeacherProfile)
+admin.site.register(Department)
+admin.site.register(College)
+
--- backend/ClockIn/api/models.py
+++ backend/ClockIn/api/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class College(models.Model):
+    name=models.CharField(max_length=200)
+    code=models.CharField(max_length=50,unique=True)
+    email=models.EmailField()
+    address=models.TextField()
+    joined_at=models.DateTimeField(auto_now_add=True)
+    ##end date will be discussed
+    def __str__(self):
+        return self.name
+    
+class Department(models.Model):
+    college=models.ForeignKey(
+        College,on_delete=models.CASCADE
+    )
+    name=models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+    
+    
+class User(AbstractUser):
+    ROLE_CHOICES=(
+        ("ADMIN","Admin"),
+        ("TEACHER","Teacher"),
+        ("STUDENT","Student"),
+    )
+    role=models.CharField(max_length=20,choices=ROLE_CHOICES)
+    college=models.ForeignKey(
+        College,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    
+    def __str__(self):
+        return self.username
+    
+    
+class StudentProfile(models.Model):
+    user=models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    department=models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE
+    )
+    enrollment_no=models.CharField(
+        max_length=50,
+        unique=True
+    )
+    division=models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.user.username
+    
+class TeacherProfile(models.Model):
+    user=models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    department=models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE
+    )
+    employee_id=models.CharField(
+        max_length=20,
+        unique=True
+    )
+    def __str__(self):
+        return self.user.username
