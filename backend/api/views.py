c1d72dbfe15322a82320bd797eb3146c9783bac0
Gaurav Sawant <gauravsawant8217@gmail.com>
2026-09-04 16:07:43 +0530

    protected all the necessary api endpoints

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,10 @@ from rest_framework import viewsets, generics
 from django.shortcuts import render,HttpResponse
 from .models import StudentProfile, TeacherProfile, College, Department,Beacon,AttendanceSession,Attendance, User
 from .serializers import StudentProfileSerializer, TeacherProfileSerializer, CollegeSerializer, DepartmentSerializer,BeaconSerializer,AttendanceSessionsSerializer,AttendanceSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticated
+
+
+
 # Create your views here.
 def home(request):
    return HttpResponse("RONIT CHAKKA HAI")
@@ -9,10 +13,12 @@ def home(request):
 class StudentViewSet(viewsets.ModelViewSet):
    queryset = StudentProfile.objects.all()
    serializer_class = StudentProfileSerializer
+   permission_classes = [IsAuthenticated]
 
 class TeacherViewSet(viewsets.ModelViewSet):
    queryset = TeacherProfile.objects.all()
    serializer_class = TeacherProfileSerializer
+   permission_classes = [IsAuthenticated]
 
 class CollegeViewSet(viewsets.ModelViewSet):
    queryset = College.objects.all()
@@ -29,11 +35,12 @@ class BeaconViewSet(viewsets.ModelViewSet):
 class AttendanceSessionsViewSet(viewsets.ModelViewSet):
    queryset=AttendanceSession.objects.all()
    serializer_class=AttendanceSessionsSerializer
+   permission_classes = [IsAuthenticated]
    
 class AttendanceViewSet(viewsets.ModelViewSet):
    queryset=Attendance.objects.all()
    serializer_class=AttendanceSerializer
-   
+   permission_classes = [IsAuthenticated]
    
 class UserCreateViewSet(generics.CreateAPIView):
    queryset = User.objects.all()
