27c9a88725bfc8ba8b1931a90981a47de96a5311
Gaurav Sawant <gauravsawant8217@gmail.com>
2026-09-09 09:54:24 +0530

    mark attendance view and format changes

--- backend/api/services.py
+++ backend/api/services.py
@@ -3,6 +3,7 @@ from rest_framework.exceptions import ValidationError
 
 from .models import Attendance, AttendanceSession
 
+
 def mark_attendance(student, token):
     # find session using qr token
     try:
@@ -19,16 +20,13 @@ def mark_attendance(student, token):
         raise ValidationError("QR already expired")
 
     # if already attendance marked
-    if Attendance.objects.filter(
-        student=student,
-        session=session
-    ).exists():
+    if Attendance.objects.filter(student=student, session=session).exists():
         raise ValidationError("Attendance already marked")
 
     # create attendance if all checks failed
     attendance = Attendance.objects.create(
-        session=session,
-        student=student,
+        session=session, 
+        student=student, 
         marked_at=timezone.now()
     )
 
